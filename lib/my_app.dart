6cedfa412818862aee04aed1e8e8744e793935ae
androsgithub <andreemaprinc@gmail.com>
2024-08-26 23:37:35 -0300

    feat: create a widget just for MyApp

---
+++ lib/my_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:viacep_flutter/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Via Cep',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
